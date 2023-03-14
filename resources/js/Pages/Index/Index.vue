aaf6f8735fe7087643d4e23f9fb5f81c7b0906f1
Mohamad Salimi <contact@salimi.my>
2023-03-15 02:46:24 +0800

    Add text above search bar

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -6,8 +6,9 @@
     <div class="container h-[30rem] flex justify-center items-center">
 
       <form class="w-96 md:w-[40rem]">
+        <h1 class="text-4xl text-white font-bold text-center mb-4 drop-shadow-lg">Find Your Next House</h1>
         <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-        <div class="relative">
+        <div class="relative drop-shadow-lg">
           <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
             <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor"
               viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -17,7 +18,7 @@
           </div>
           <input type="search" id="default-search"
             class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Search Houses..." required>
+            placeholder="Search for Houses..." required>
           <button type="submit"
             class="text-white absolute right-2.5 bottom-[9.5px] bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
         </div>
