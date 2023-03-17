21713ccde19fa0bc850583f32de16bc4a50dda5e
Mohamad Salimi <contact@salimi.my>
2023-03-17 23:23:06 +0800

    Add border bottom on nav

--- resources/js/Layouts/Layout.vue
+++ resources/js/Layouts/Layout.vue
@@ -27,7 +27,8 @@ const flashSuccess = computed(() => usePage().props.flash.success)
   </Head>
   <main>
     <header>
-      <nav class="bg-white px-2 sm:px-4 py-2 md:py-1 fixed w-full z-20 top-0 left-0 dark:bg-gray-900 drop-shadow-md">
+      <nav
+        class="bg-white px-2 sm:px-4 py-2 md:py-1 fixed w-full z-20 top-0 left-0 dark:bg-gray-800 drop-shadow-md border-b dark:border-gray-700">
         <div class="container flex flex-wrap items-center justify-between mx-auto">
           <a href="https://flowbite.com/" class="flex items-center">
             <img src="/assets/images/house-logo.png" class="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
@@ -74,7 +75,7 @@ const flashSuccess = computed(() => usePage().props.flash.success)
           </div>
           <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
             <ul
-              class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-900 md:dark:bg-gray-800 dark:border-gray-700">
               <li>
                 <a href="#"
                   class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
