459d9fdabc7c8b94484807718fe84b2284608654
Mohamad Salimi <contact@salimi.my>
2023-03-21 15:34:20 +0800

    Add user logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -32,7 +32,12 @@ class AuthController extends Controller
         return redirect()->intended('listing');
     }
 
-    public function destroy()
+    public function destroy(Request $request)
     {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/');
     }
 }
--- resources/js/Layouts/Layout.vue
+++ resources/js/Layouts/Layout.vue
@@ -1,12 +1,13 @@
 <script setup>
 import { Head, Link, usePage } from '@inertiajs/vue3'
-import { computed, onMounted } from 'vue'
+import { computed, onMounted, watch } from 'vue'
 import { initCollapses, initTooltips } from 'flowbite'
 import { useDark, useToggle } from '@vueuse/core'
+import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue'
 
 onMounted(() => {
-  initCollapses();
-  initTooltips();
+  initCollapses()
+  initTooltips()
 })
 
 const isDark = useDark()
@@ -18,6 +19,11 @@ const props = defineProps({
 })
 
 const flashSuccess = computed(() => usePage().props.flash.success)
+const user = computed(() => usePage().props.user)
+
+watch(user, () => {
+  //
+})
 
 const d = new Date();
 let year = d.getFullYear();
@@ -60,11 +66,66 @@ let year = d.getFullYear();
               <div class="tooltip-arrow" data-popper-arrow=""
                 style="position: absolute; left: 0px; transform: translate(69px, 0px);"></div>
             </div>
-            <Link href="/login"
+
+            <Menu v-if="user" as="div" class="relative inline-block text-left self-center">
+              <div>
+                <MenuButton
+                  class="flex items-center text-sm font-medium p-1 text-gray-700 rounded-full hover:bg-gray-100 dark:hover:bg-gray-700 hover:text-blue-700 dark:hover:text-blue-500 md:mr-0 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:text-white">
+                  <span class="sr-only">Open user menu</span>
+                  <img class="w-8 h-8 mr-2 rounded-full object-cover" src="/assets/images/avatar.svg" alt="user photo">
+                  <span class="w-0 lg:w-full truncate lg:mr-2">{{ user?.name }}</span>
+                  <i class="fa-solid fa-caret-down"></i>
+                </MenuButton>
+              </div>
+
+              <transition enter-active-class="transition duration-100 ease-out"
+                enter-from-class="transform scale-95 opacity-0" enter-to-class="transform scale-100 opacity-100"
+                leave-active-class="transition duration-75 ease-in" leave-from-class="transform scale-100 opacity-100"
+                leave-to-class="transform scale-95 opacity-0">
+                <MenuItems
+                  class="absolute right-0 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-lg shadow w-52 dark:bg-gray-700 dark:divide-gray-600">
+                  <div class="px-4 py-3 text-sm text-gray-900 dark:text-white">
+                    <div class="font-medium">{{ user?.name }}</div>
+                    <div class="truncate">{{ user?.email }}</div>
+                  </div>
+                  <div class="px-1 py-1">
+                    <MenuItem v-slot="{ active }">
+                    <button :class="[
+                      active ? 'bg-gray-100 dark:bg-gray-600 dark:text-white' : 'text-gray-700 dark:text-gray-200',
+                      'group flex w-full items-center rounded-md px-4 py-2 text-sm',
+                    ]">
+                      Dashboard
+                    </button>
+                    </MenuItem>
+                    <MenuItem v-slot="{ active }">
+                    <button :class="[
+                      active ? 'bg-gray-100 dark:bg-gray-600 dark:text-white' : 'text-gray-700 dark:text-gray-200',
+                      'group flex w-full items-center rounded-md px-4 py-2 text-sm',
+                    ]">
+                      Settings
+                    </button>
+                    </MenuItem>
+                  </div>
+                  <div class="px-1 py-1">
+                    <MenuItem v-slot="{ active }">
+                    <Link href="/logout" :class="[
+                      active ? 'bg-gray-100 dark:bg-gray-600 dark:text-white' : 'text-gray-700 dark:text-gray-200',
+                      'group flex w-full items-center rounded-md px-4 py-2 text-sm',
+                    ]" method="delete" as="button">
+                    Sign out
+                    </Link>
+                    </MenuItem>
+                  </div>
+                </MenuItems>
+              </transition>
+            </Menu>
+
+            <Link v-else href="/login"
               class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-2 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-0 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             <span class="mr-2">Sign in</span>
             <i class="fa-solid fa-right-to-bracket"></i>
             </Link>
+
             <button data-collapse-toggle="navbar-cta" type="button"
               class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 ml-1"
               aria-controls="navbar-cta" aria-expanded="false">
