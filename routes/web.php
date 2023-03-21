8cb58db5aaac87c323f9edd07ec38f0188395826
Mohamad Salimi <contact@salimi.my>
2023-03-21 15:51:11 +0800

    Protect rroute for authenticated user

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [IndexController::class, 'index']);
 
-Route::resource('listing', ListingController::class);
+Route::resource('listing', ListingController::class)->only(['create', 'store', 'edit', 'update', 'destroy'])->middleware('auth');
+Route::resource('listing', ListingController::class)->except(['create', 'store', 'edit', 'update', 'destroy']);
 
 Route::get('login', [AuthController::class, 'create'])->name('login');
 Route::post('login', [AuthController::class, 'store'])->name('login.store');
