18557087d0824ce90652ba5b710a1f5143f7ae41
Mohamad Salimi <contact@salimi.my>
2023-03-28 06:34:09 +0800

    Move svg into component

--- resources/js/Layouts/Layout.vue
+++ resources/js/Layouts/Layout.vue
@@ -4,6 +4,9 @@ import { computed, onMounted } from 'vue'
 import { initCollapses, initTooltips } from 'flowbite'
 import { useDark, useToggle } from '@vueuse/core'
 import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue'
+import Bars from '../Components/SVG/Bars.vue'
+import Moon from '../Components/SVG/Moon.vue'
+import Sun from '../Components/SVG/Sun.vue'
 
 onMounted(() => {
   initCollapses()
@@ -41,16 +44,8 @@ let year = d.getFullYear();
           <div class="flex md:order-2">
             <button @click="toggleDark()" id="theme-toggle" data-tooltip-target="tooltip-toggle" type="button"
               class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1">
-              <svg v-if="!isDark" aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg">
-                <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
-              </svg>
-              <svg v-else aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-                  fill-rule="evenodd" clip-rule="evenodd"></path>
-              </svg>
+              <Moon v-if="!isDark" :class="'w-5 h-5'" />
+              <Sun v-else :class="'w-5 h-5'" />
               <span class="sr-only">Switch Mode</span>
             </button>
             <div id="tooltip-toggle" role="tooltip"
@@ -134,12 +129,7 @@ let year = d.getFullYear();
               class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 ml-1"
               aria-controls="navbar-cta" aria-expanded="false">
               <span class="sr-only">Open main menu</span>
-              <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd"
-                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clip-rule="evenodd"></path>
-              </svg>
+              <Bars :class="'w-6 h-6'" />
             </button>
           </div>
           <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
