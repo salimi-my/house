e460a05f59317a54931be9fbc79ea3b15ef024a1
Mohamad Salimi <contact@salimi.my>
2023-03-28 01:06:13 +0800

    Change hr color on dark mode

--- resources/js/Components/ProfileCard.vue
+++ resources/js/Components/ProfileCard.vue
@@ -31,7 +31,7 @@ const user = computed(() => usePage().props.user)
           class="w-full mb-2 text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2 text-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">
         Listings
         </Link>
-        <hr class="my-2" />
+        <hr class="my-2 dark:border-gray-600" />
         <Link href="/logout" method="delete" as="button"
           class="w-full my-2 text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2 text-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">
         <i class="fa-solid fa-right-from-bracket"></i> Sign out
