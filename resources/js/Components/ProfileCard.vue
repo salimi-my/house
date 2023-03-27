8fe3e28bda8fcae3d54e0b96b953efd5c4354d65
Mohamad Salimi <contact@salimi.my>
2023-03-28 01:00:59 +0800

    Add listing button into profile card

--- resources/js/Components/ProfileCard.vue
+++ resources/js/Components/ProfileCard.vue
@@ -27,6 +27,10 @@ const user = computed(() => usePage().props.user)
           :class="[setting ? 'bg-blue-700 text-white dark:text-white' : '', 'w-full mb-2 text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2 text-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800']">
         Settings
         </Link>
+        <Link href="/user-account/my-listing" as="button"
+          class="w-full mb-2 text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2 text-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">
+        Listings
+        </Link>
         <hr class="my-2" />
         <Link href="/logout" method="delete" as="button"
           class="w-full my-2 text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2 text-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">
